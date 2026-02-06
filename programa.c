e73eb40e177494d24e9e7846786cc04998383a1a
dimasmsoares <35304959+dimasmsoares@users.noreply.github.com>
2026-02-06 14:30:31 +0000

    Identificação do Servidor

--- programa.c
+++ programa.c
@@ -5,29 +5,52 @@
 #include <unistd.h>
 #include <cjson/cJSON.h>
 
+/* Essa estrutura armazena os dados extraidos do JSON dos 
+funcionários da Câmara, disponível no Dados Abertos */
+typedef struct {
+    char ponto[7];
+    char nome[80];
+    char cargo[80];
+    char funcao[120];
+
+} InfosServidor;
+
+/* PROTÓTIPO DAS FUNÇÕES */
+char *obter_ponto();
+int pesquisar_infos_servidor(InfosServidor *info_servidor);
 int menu(const char *titulo, const char *opcoes[], int n_opçoes, const char *descricao);
-int formulario_infos_servidor();
 int tecla_pressionada();
+int tecla_pressionada_com_echo();
 char *ler_arquivo(const char *nome);
 
-
 int main(int argc, char *argv[]){
-    formulario_infos_servidor();
-    /*const char *opcoes[] =    {"Simular contracheque", 
-                                "Visualizar estruturas remuneratórias", 
-                                "Projetar evolução na carreira", 
-                                "Sair"};
-    int n_opcoes = 4;
-    const char *desc = "Selecione uma opção abaixo:\n";
+    InfosServidor info_servidor = {0};  // Inicializa a struct info_servidor
 
-    menu("CÂMARA DOS DEPUTADOS", opcoes, n_opcoes, desc);
-    system("clear");
-    */
+    char *p = obter_ponto();   // Obtem o ponto do servidor
+    strcpy(info_servidor.ponto, p);     // Grava o ponto na struct info_servidor
+    free(p);                            
+
+    pesquisar_infos_servidor(&info_servidor);   // A partir do ponto, pesquisa as informaçoes do servidor
+                                                // no JSON e armazena elas na struct info_servidor
+
+    
+    const char *titulo = "CÂMARA DOS DEPUTADOS";
+    const char *opcoes[] = {"Simular contrachaque", 
+                            "Ver minhas informações"};
+    int n_opcoes = 2;
+    char descricao[80] = "Olá, ";
+    int pos = strcspn(info_servidor.nome, " ");
+    char nome[20] = {'\0'};    
+    strncpy(nome, info_servidor.nome, pos);
+    strcat(descricao, nome);
+    strcat(descricao, ". O que deseja?");
+
+    int opcao = menu(titulo, opcoes, n_opcoes, descricao);
+
+    printf("%d\n", opcao);
     return 0;
+
 }
-
-
-
 
 int menu(const char *titulo, const char *opcoes[], int n_opçoes, const char *descricao){
     int selecionado = 0;
@@ -37,7 +60,7 @@ int menu(const char *titulo, const char *opcoes[], int n_opçoes, const char *de
         system("clear");
         
         //Imprime o título do menu
-        printf("##### %s #####\n\n", titulo);
+        printf("########## %s ##########\n\n", titulo);
 
         printf("%s\n", descricao);
 
@@ -88,11 +111,8 @@ int menu(const char *titulo, const char *opcoes[], int n_opçoes, const char *de
     }   // while
 }   // menu
 
-int formulario_infos_servidor(){
-    const char *nome = "funcionarios.json";
-    const char *p = "P_6843";
-
-    char *conteudo = ler_arquivo(nome);
+int pesquisar_infos_servidor(InfosServidor *info_servidor){
+    char *conteudo = ler_arquivo("funcionarios.json");
     if (!conteudo) {
         printf("Erro ao ler arquivo\n");
         return 1;
@@ -119,13 +139,16 @@ int formulario_infos_servidor(){
         cJSON *ponto = cJSON_GetObjectItem(item, "ponto");
         cJSON *nome  = cJSON_GetObjectItem(item, "nome");
         cJSON *cargo = cJSON_GetObjectItem(item, "cargo");
+        cJSON *funcao = cJSON_GetObjectItem(item, "funcao");
 
         if (cJSON_IsString(ponto) &&
-            strcmp(ponto->valuestring, p) == 0) {
-
-            printf("✅ Encontrado!\n");
-            printf("Nome: %s\n", nome ? nome->valuestring : "(sem nome)");
-            printf("Cargo: %s\n", cargo ? cargo->valuestring : "(sem cargo)");
+            strcmp(ponto->valuestring, info_servidor->ponto) == 0) {
+            
+            strcpy(info_servidor->nome, nome->valuestring);
+            strcpy(info_servidor->cargo, cargo->valuestring);
+            strcpy(info_servidor->funcao, funcao->valuestring);
+            //printf("Nome: %s\n", nome ? nome->valuestring : "(sem nome)");
+            //printf("Cargo: %s\n", cargo ? cargo->valuestring : "(sem cargo)");
             break;
         }
     }
@@ -172,7 +195,46 @@ int tecla_pressionada(){
     return ch;
 }   // tecla_pressionada
 
+int tecla_pressionada_com_echo(){
+    /*No Linux/Unix, a função _getch() existente no Windows não existe por padrão. O terminal funciona em modo "canônico" 
+    (espera uma linha inteira e o "Enter"). Para simular o comportamento do Windows, precisamos manipular as configurações 
+    do terminal manualmente usando a estrutura "termios".*/
+
+    // Cria duas variáveis para guardar as configurações do terminal: oldt (configuração antiga/original) e newt (nova 
+    // configuração que vamos criar).
+    struct termios oldt, newt;  
+    
+    // Variável para armazenar o caractere pressionado
+    int ch;
+    
+    // Pega (get) os atributos atuais do terminal e salva em oldt. Esses atributos definem o comportamento do terminal.
+    // Ex: (ICANON) Esperar o ENTER para a execução do comando.
+    tcgetattr(STDIN_FILENO, &oldt);
+    
+    // Copia as configurações originais para a variável newt. Vamos modificar essa cópia.
+    newt = oldt;
+    
+    // Desligando duas "bandeiras" (flags) de configuração usando operações bit a bit:
+    newt.c_lflag &= ~(ICANON);
+    
+    // Aplica (set) as novas configurações ao terminal agora (TCSANOW). A partir deste momento, o terminal para de 
+    // esperar o Enter e de mostrar letras.
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    // Lê o caractere pressionado e guarda na variável ch. 
+    ch = getchar();
+
+    // Restaura as configurações originais (oldt)
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    // Retorna o caractere lido para quem chamou a função.
+    return ch;
+}   // tecla_pressionada
+
 char *ler_arquivo(const char *nome) {
+    /* Essa função abre um arquivo JSON, ler seu conteúdo 
+    e o copia para um buffer que é retornado */
+    
     FILE *f = fopen(nome, "r");
     if (!f) return NULL;
 
@@ -186,4 +248,35 @@ char *ler_arquivo(const char *nome) {
 
     fclose(f);
     return buffer;
+}
+
+char *obter_ponto(){
+    /* Esta função imprime uma mensagem na tela, solicitando que o usuário digite seu numéro de ponto*/
+    
+    char c; // Armazena oa caractere pressionado
+
+    char *ponto = malloc(7);    // Armazena o ponto
+    strcpy(ponto, "P_0000");
+
+    system("clear");    //Limpa a tela
+    
+    // Imprimindo a mensagem
+    printf("########## CÂMARA DOS DEPUTADOS ##########");
+    printf("\nInsira seu ponto: P_");
+    
+    for(int i = 2; i < 6; i++){
+        c = tecla_pressionada_com_echo();
+        if(c < '0' || c > '9'){
+            printf("\033[D"); // move cursor para a esquerda
+            printf("\033[K"); // apaga até o fim da linha
+            i--;
+        }
+        else{
+            ponto[i] = c;
+        }    
+    }
+
+    ponto[6] = '\0';
+    
+    return ponto;
 }
